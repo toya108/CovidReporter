ad634d57d5ed881d9a50ab02b3f85c106ba5bd7b
toya108 <tk108gabalian@gmail.com>
2021-11-03 22:02:14 +0900

    first commit

---
+++ CovidReporter/CovidReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidReporterApp.swift
+//  CovidReporter
+//
+//  Created by TOUYA KAWANO on 2021/11/03.
+//
+
+import SwiftUI
+
+@main
+struct CovidReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
