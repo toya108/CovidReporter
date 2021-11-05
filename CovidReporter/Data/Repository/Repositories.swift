f687585e9d8b006d4f2f70ab245e45affa3cb702
toya108 <tk108gabalian@gmail.com>
2021-11-05 23:18:26 +0900

    rename

--- CovidReporter/Data/Repository/Repositories.swift
+++ CovidReporter/Data/Repository/Repositories.swift
@@ -1,5 +1,5 @@
 struct Repositories {
-    struct InfectedPeopleNumbers {
-        typealias Get = Repository<GetInfectedPeopleNumbers>
+    struct InfectionNumbers {
+        typealias Get = Repository<GetInfectionNumbers>
     }
 }
