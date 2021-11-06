1c2d2f58013f0c9da423282f33e24cbf855d19ab
toya108 <tk108gabalian@gmail.com>
2021-11-06 13:08:42 +0900

    delete unnecessary view

--- CovidReporter/CovidReporterApp.swift
+++ CovidReporter/CovidReporterApp.swift
@@ -1,17 +1,10 @@
-//
-//  CovidReporterApp.swift
-//  CovidReporter
-//
-//  Created by TOUYA KAWANO on 2021/11/03.
-//
-
 import SwiftUI
 
 @main
 struct CovidReporterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ChartListView()
         }
     }
 }
