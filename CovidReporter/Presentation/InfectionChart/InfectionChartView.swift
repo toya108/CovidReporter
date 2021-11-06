2a219116d6de15827ec2a2aaa9c25af75a27a109
toya108 <tk108gabalian@gmail.com>
2021-11-07 00:14:17 +0900

    enable to delete prefecture

--- CovidReporter/Presentation/InfectionChart/InfectionChartView.swift
+++ CovidReporter/Presentation/InfectionChart/InfectionChartView.swift
@@ -6,10 +6,12 @@ struct InfectionChartView: View {
         getInfectionNumbersRepository: .init(),
         getAllInfectionNumbersRepository: .init()
     )
+    @ObservedObject private var parentViewModel: ChartListViewModel
     private let prefecture: Prefecture
 
-    init(prefecture: Prefecture) {
+    init(prefecture: Prefecture, parentViewModel: ChartListViewModel) {
         self.prefecture = prefecture
+        self.parentViewModel = parentViewModel
     }
 
     var body: some View {
@@ -20,6 +22,18 @@ struct InfectionChartView: View {
                 Text(
                     "\(viewModel.dataSource.latestDate) 新規感染者数(\(Int(viewModel.dataSource.latestAdpatients)))"
                 )
+                if prefecture != .all {
+                    Button(
+                        action: {
+                            parentViewModel.delete(prefecture: self.prefecture)
+                            parentViewModel.updatePrefectures()
+                        },
+                        label: {
+                            Image(systemName: "x.circle.fill")
+                                .foregroundColor(Color(uiColor: .systemTeal))
+                        }
+                    ).buttonStyle(.plain)
+                }
             }
             BarChart(dataSource: $viewModel.dataSource)
                 .onAppear {
@@ -34,6 +48,13 @@ struct InfectionChartView: View {
 
 struct InfectionChartView_Previews: PreviewProvider {
     static var previews: some View {
-        InfectionChartView(prefecture: .all)
+        InfectionChartView(
+            prefecture: .all,
+            parentViewModel: .init(
+                getPrefecturesRepository: .init(),
+                setPrefecturesRepository: .init(),
+                deletePrefecturesRepository: .init()
+            )
+        )
     }
 }
