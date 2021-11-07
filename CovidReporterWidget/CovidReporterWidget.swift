b3e9f616d7f43be70fcd402c9c02caaa440fe40b
toya108 <tk108gabalian@gmail.com>
2021-11-07 21:55:46 +0900

    add wedgit

---
+++ CovidReporterWidget/CovidReporterWidget.swift
@@ -0,0 +1,111 @@
+import WidgetKit
+import SwiftUI
+import Intents
+import SwiftUICharts
+
+struct Provider: IntentTimelineProvider {
+
+    private let getAllInfectionNumbersRepository = Repositories.InfectionNumbers.All.Get()
+
+    func placeholder(in context: Context) -> InfectionNumberChartEntry {
+        InfectionNumberChartEntry(
+            date: Date(),
+            dataSource: .init(),
+            prefecture: .all
+        )
+    }
+
+    func getSnapshot(
+        for configuration: ConfigurationIntent,
+        in context: Context,
+        completion: @escaping (InfectionNumberChartEntry) -> Void
+    ) {
+
+        Task {
+            do {
+                let infectionNumbers = try await getAllInfectionNumbersRepository.request()
+                let latest = Array(infectionNumbers.suffix(7))
+                let entry = InfectionNumberChartEntry(
+                    date: Date(),
+                    dataSource: .init(dataEntryConvertibles: latest),
+                    prefecture: .all
+                )
+                completion(entry)
+
+            } catch {
+
+            }
+        }
+    }
+
+    func getTimeline(
+        for configuration: ConfigurationIntent,
+        in context: Context,
+        completion: @escaping (Timeline<Entry>) -> Void
+    ) {
+
+        Task {
+            do {
+                let infectionNumbers = try await getAllInfectionNumbersRepository.request()
+                let latest = Array(infectionNumbers.suffix(7))
+                let entry = InfectionNumberChartEntry(
+                    date: Date(),
+                    dataSource: .init(dataEntryConvertibles: latest),
+                    prefecture: .all
+                )
+                let timeline = Timeline(
+                    entries: [entry],
+                    policy: .after(Date().addingTimeInterval(60 * 10))
+                )
+                completion(timeline)
+            } catch {
+
+            }
+        }
+    }
+}
+
+struct InfectionNumberChartEntry: TimelineEntry {
+    let date: Date
+    let dataSource: BarChartDataSource
+    let prefecture: Prefecture
+}
+
+struct CovidReporterWidgetEntryView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        InfectionChartMediumWidgetView(entry: entry)
+    }
+}
+
+@main
+struct CovidReporterWidget: Widget {
+    let kind: String = "CovidReporterWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(
+            kind: kind,
+            intent: ConfigurationIntent.self,
+            provider: Provider()
+        ) { entry in
+            CovidReporterWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("コロミル")
+        .description("コロナの新規感染者数をいつでもチェック")
+        .supportedFamilies([.systemMedium])
+    }
+}
+
+ struct CovidReporterWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        CovidReporterWidgetEntryView(
+            entry: InfectionNumberChartEntry(
+                date: Date(),
+                dataSource: .init(),
+                prefecture: .all
+            )
+        )
+        .previewContext(WidgetPreviewContext(family: .systemMedium))
+    }
+ }
---
+++ CovidReporterWidget/InfectionChartMediumWidgetView.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+import SwiftUICharts
+import WidgetKit
+
+ struct InfectionChartMediumWidgetView: View {
+
+    var entry: InfectionNumberChartEntry
+
+    init(entry: InfectionNumberChartEntry) {
+        self.entry = entry
+    }
+
+     var body: some View {
+         GeometryReader { content in
+             VStack {
+                 HStack {
+                     Text(entry.prefecture.rawValue).fontWeight(.heavy)
+                     let latestAdaptients = "(\(Int(entry.dataSource.latestAdpatients))人)"
+                     Text(entry.dataSource.latestDate.description + "新規感染者数" + latestAdaptients)
+                         .font(.system(size: 12))
+                 }
+                 BarChart()
+                     .data(entry.dataSource.values)
+                     .chartStyle(
+                        .init(
+                            backgroundColor: .teal,
+                            foregroundColor: .init(.init(uiColor: .systemTeal))
+                        )
+                     )
+                 HStack(spacing: 16) {
+                     let width = content.size.width / CGFloat(entry.dataSource.dateStrings.count)
+                     ForEach(entry.dataSource.dateStrings, id: \.self) {
+                         Text($0).font(.system(size: 11))
+                             .frame(maxWidth: width)
+                     }
+                 }
+             }
+             .padding(16)
+         }
+     }
+ }
+
+ struct InfectionChartMediumWidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfectionChartMediumWidgetView(
+            entry: .init(
+                date: Date(),
+                dataSource: .init(),
+                prefecture: .all
+            )
+        )
+        .previewContext(WidgetPreviewContext(family: .systemMedium))
+    }
+ }
