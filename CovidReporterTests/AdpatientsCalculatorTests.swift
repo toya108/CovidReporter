7de14b59b6391939aeca84978ae2bef955788c96
toya108 <tk108gabalian@gmail.com>
2021-11-12 00:26:06 +0900

    fix tests

--- CovidReporterTests/AdpatientsCalculatorTests.swift
+++ CovidReporterTests/AdpatientsCalculatorTests.swift
@@ -4,12 +4,12 @@ import XCTest
 class AdpatientsCalculatorTests: XCTestCase {
 
     func testAddAdatients() throws {
-        let sources: [InfectionNumbers] = [
-            .init(itemList: [.init(date: "2021-11-01", nameJp: "北海道", npatients: "1000")]),
-            .init(itemList: [.init(date: "2021-11-02", nameJp: "北海道", npatients: "2000")]),
-            .init(itemList: [.init(date: "2021-11-03", nameJp: "北海道", npatients: "3000")]),
-            .init(itemList: [.init(date: "2021-11-04", nameJp: "北海道", npatients: "4000")]),
-            .init(itemList: [.init(date: "2021-11-05", nameJp: "北海道", npatients: "5000")])
+        let sources: [InfectionNumbers.Item] = [
+            .init(date: "2021-11-01", nameJp: "北海道", npatients: "1000"),
+            .init(date: "2021-11-02", nameJp: "北海道", npatients: "2000"),
+            .init(date: "2021-11-03", nameJp: "北海道", npatients: "3000"),
+            .init(date: "2021-11-04", nameJp: "北海道", npatients: "4000"),
+            .init(date: "2021-11-05", nameJp: "北海道", npatients: "5000")
         ]
 
         let infectionNumbersPerDay = AdpatientsCalculator.addAdpatients(from: sources)
